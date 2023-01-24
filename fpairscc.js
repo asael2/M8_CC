409b4bab0c4ea243b52870d6d51eb0c52d607568
Asael Arenas <asael2@gmail.com>
2023-01-24 12:58:44 -0500

    Update fpairscc.js
    Compatibility ECMAScript 6.

--- fpairscc.js
+++ fpairscc.js
@@ -1,8 +1,8 @@
 function findPairs(nums, target) {
-  var pairs = [];
-  var numsMap = {};
-  for (var i = 0; i < nums.length; i++) {
-    var complement = target - nums[i];
+  let pairs = [];
+  let numsMap = {};
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
     if (numsMap[complement] !== undefined) {
       pairs.push([complement, nums[i]]);
     }
